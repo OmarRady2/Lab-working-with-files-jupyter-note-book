be89f79d03604efc690d61c678eb8c5832547a5f
OmarRady2 <138314140+OmarRady2@users.noreply.github.com>
2023-08-02 20:32:52 +0300

    Lab: working with files in jupyter

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# in this notebook we are going to talk about Data science (tools, languages, libraries, Objevtives) used in the course
+
+# some of the popular data science languages are:
+# 1- python                                                                                                                       
+# 2-R                                                                                                                             
+# 3-SQL                                                                                                                           
+# 4-julia                                                                                                                      
+# 5-JAVA                                                                                                                             
+# there is more like javascript, C, GO, Scala and many more
+
+# most used libraries in data science are:  
+# matplotlib, pandas, ggplot, kers, Tensorflow, SciPy, NumPy, and many more
+
+# most used data science tools are:                                                                                                   
+# Apache Spark, Jupyter, Apache Hadoop and many more
+
+# ###  some artihmetic expressions in python
+# 4 + 4  for addition
+# 
+# 4 - 4  for subtraction
+# 
+# 4 / 4  for dividing 
+# 
+# 4 * 4  for Multiplication
+# 
+# 4 % 4  for mod
+# 
+
+# In[1]:
+
+
+# trying arithmetic expression
+(3*4)+5
+
+
+# In[3]:
+
+
+#converting 200 minutes to hours
+(200/60)
+
+
+# ###### Objectives 
+# knowing about popular languages in data science like: python, scala, r
+# 
+# knowing about popular liberies in data science like: matplotlib, ggplot
+# 
+# knowing about popular tools in data science like: apache spark, apache hadoop
+
+# ## Author
+# Omar Ahmed Rady
+
+# In[ ]:
+
+
+
+
